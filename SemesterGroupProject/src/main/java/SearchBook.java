bc85e34dd16a62dd20c1a66ec407df417cfee2a6
pjstudent <93747727+PrasannaJC@users.noreply.github.com>
2022-04-09 22:03:51 +0000

    SearchBook file has been updated - will eventually shift this content into the Booksearch file

--- SemesterGroupProject/src/main/java/SearchBook.java
+++ SemesterGroupProject/src/main/java/SearchBook.java
@@ -39,12 +39,37 @@ public class SearchBook extends HttpServlet {
 		String keyCopy = request.getParameter("copies"); //label it as whatever is designated in the HTML
 		String keyYear = request.getParameter("date"); //label it as whatever is designated in the HTML
 		
-		try {
-			searching(response, keyTitle, keyAuthor, keyISBN, keyCategory, keyCopy, keyYear);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		response.setContentType("text/html");
+	    PrintWriter out = response.getWriter();
+	    String title = "Database Result";
+	    String docType = "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n"; //
+	    out.println(docType + //
+	        "<html>\n" + //
+	        "<head><title>" + title + "</title></head>\n" + //
+	        "<body bgcolor=\"#f0f0f0\">\n" + //
+	        "<h1 align=\"center\">" + title + "</h1>\n");
+	    out.println("<ul>");
+	
+	    List<Book> bkz = null;
+	    if ((keyTitle != null && !keyTitle.isEmpty()) || (keyAuthor != null && !keyAuthor.isEmpty()) || (keyISBN != null && !keyISBN.isEmpty()) 
+	   		  || (keyCategory != null && !keyCategory.isEmpty()) || (keyCopy != null && !keyCopy.isEmpty()) || (keyYear != null && !keyYear.isEmpty()))
+	    {
+		    bkz = BookUtil.listBooks(keyTitle, keyAuthor, keyISBN, keyCategory, keyCopy, keyYear);
+	    	//bkz = BookUtil.listBooks();
+	    	out.println("the if part");
+	    }
+	    else
+	    {
+	  	   bkz = BookUtil.listBooks();
+	    	out.println("the else part");
+	    }
+	    display(bkz, out);
+	    out.println("</ul>");
+	    out.println("<a href=/" + Info.projectName + "/" + Info.searchWebName + ">Searching for a book...</a> <br>");
+	    //out.println("<a href=/" + projectName + "/" + searchWebName + ">Search Data</a> <br>");
+	    out.println("</body></html>");
+			
+		
 	}
 	
 	
@@ -53,215 +78,23 @@ public class SearchBook extends HttpServlet {
 	      for (Book b : listBooks) {
 	         System.out.println("[DBG] " + b.getTitle() + ", " //
 	        		 + b.getAuthor() + ", " //
-	               + b.getISBN13() + ", " //
-	        		+ b.getCategory() + ", " //
-	        		+ b.getCopies() + ", " //
-	        		+ b.getDescription() + ", " //
-	        		+ b.getPublicationYear());
+	        		 + b.getISBN13() + ", " //
+	        		 + b.getCategory() + ", " //
+	        		 + b.getCopies() + ", " //
+	        		 + b.getDescription() + ", " //
+	        		 + b.getPublicationYear());
 
 	         out.println("<li>" + b.getTitle() + ", " //
 	        		 + b.getAuthor() + ", " //
-		               + b.getISBN13() + ", " //
-		        		+ b.getCategory() + ", " //
-		        		+ b.getCopies() + ", " //
-		        		+ b.getDescription() + ", " //
-		        		+ b.getPublicationYear() + "</li>");
+	        		 + b.getISBN13() + ", " //
+	        		 + b.getCategory() + ", " //
+	        		 + b.getCopies() + ", " //
+	        		 + b.getDescription() + ", " //
+	        		 + b.getPublicationYear() + "</li>");
 	      }
 	   }
-	
-	
-	void searching(HttpServletResponse response, String keyTitle, String keyAuthor, String keyISBN, String keyCategory, String keyCopy, String keyYear) throws IOException
-	{
-		 response.setContentType("text/html");
-	      PrintWriter out = response.getWriter();
-	      String title = "Database Result";
-	      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n"; //
-	      out.println(docType + //
-	            "<html>\n" + //
-	            "<head><title>" + title + "</title></head>\n" + //
-	            "<body bgcolor=\"#f0f0f0\">\n" + //
-	            "<h1 align=\"center\">" + title + "</h1>\n");
-	      out.println("<ul>");
 
-	      List<Book> bkz = null;
-	      if ((keyTitle != null && !keyTitle.isEmpty()) || (keyAuthor != null && !keyAuthor.isEmpty()) || (keyISBN != null && !keyISBN.isEmpty()) 
-	    		  || (keyCategory != null && !keyCategory.isEmpty()) || (keyCopy != null && !keyCopy.isEmpty()) || (keyYear != null && !keyYear.isEmpty()))
-	      {
-	    	  bkz = BookUtil.listBook(keyTitle, keyAuthor, keyISBN, keyCategory, keyCopy, keyYear);
-	      }
-	      else
-	      {
-	    	  bkz = BookUtil.listBooks();
-	      }
-	      display(bkz, out);
-	      out.println("</ul>");
-	      out.println("<a href=/" + Info.projectName + "/" + Info.searchWebName + ">Searching for a book...</a> <br>");
-	      //out.println("<a href=/" + projectName + "/" + searchWebName + ">Search Data</a> <br>");
-	      out.println("</body></html>");
-	}
 	
-	
-	/**
-	 * 
-	 * @param response
-	 * @param keyTitle
-	 * @param keyAuthor
-	 * @param keyISBN
-	 * @param keyCategory
-	 * @param keyCopy
-	 * @param keyYear
-	 * @throws SQLException
-	 * @throws IOException
-	 */
-	@SuppressWarnings("null")
-	/*
-	 * The Master search method:
-	 * Based upon when certain conditions are met, different lesser search methods will be called upon.
-	 */
-	/*
-	void masterSearch(HttpServletResponse response, String keyTitle, String keyAuthor, String keyISBN, String keyCategory, String keyCopy, String keyYear) throws SQLException, IOException
-	{
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-		PrintWriter out = response.getWriter();
-		
-        String title = "Database Result";
-        String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + //
-              "transitional//en\">\n"; //
-        out.println(docType + //
-              "<html>\n" + //
-              "<head><title>" + title + "</title></head>\n" + //
-              "<body bgcolor=\"#f0f0f0\">\n" + //
-              "<h1 align=\"center\">" + title + "</h1>\n");
-        
-        DBconnecter.getDBConnection(getServletContext());
-        connection = DBconnecter.connection;
-        
-		String keys[] = {keyTitle, keyAuthor, keyISBN, keyCategory, keyCopy, keyYear};
-		String keyNames[] = {"TITLE", "AUTHOR", "ISBN13", "CATEGORY", "COPIES", "PublicationYear"};
-		String selectSQL = "SELECT * FROM BookTable";
-		Boolean test[] = {false, false, false, false, false, false};
-		
-
-		
-		for (int i = 0; i < keys.length; i++)
-		{	
-			if (keys[i] != "" || !keys[i].isEmpty()) // Just check if this is not accidently repeating the same command
-			{
-				test[i] = true;
-			}
-			//System.out.println(keyNames[i] + " is " + test[i] + " with the input => " + keys[i] + " <=  ");
-		}
-		
-
-		
-		for (int j = 0; j < test.length; j++)
-		{
-			if (test[j] && j < 4)
-			{
-				if (String.valueOf(selectSQL.charAt(selectSQL.length() - 1)).equals("?"))
-				{
-					selectSQL += " AND ";
-				}
-				else
-				{
-					selectSQL += " WHERE ";
-				}
-				
-				selectSQL += keyNames[j] + " LIKE ?";
-				
-				
-			}
-			else if (test[j] && j == 4)
-			{
-				if (String.valueOf(selectSQL.charAt(selectSQL.length() - 1)).equals("?"))
-				{
-					selectSQL += " AND ";
-				}
-				else
-				{
-					selectSQL += " WHERE ";
-				}
-				
-				selectSQL += keyNames[4] + " >= ?";
-				
-				
-			}
-			else if (test[j] && j == 5)
-			{
-				if (String.valueOf(selectSQL.charAt(selectSQL.length() - 1)).equals("?"))
-				{
-					selectSQL += " AND ";
-				}
-				else
-				{
-					selectSQL += " WHERE ";
-				}
-				
-				selectSQL += keyNames[5] + " = ?";
-			}
-			
-		}
-	
-
-		//System.out.println("This is the selectSQL statement => " + selectSQL);
-		
-		response.setContentType("text/html");
-		
-		preparedStatement = connection.prepareStatement(selectSQL);
-		int q = 1;
-		String temp;
-		for (int p = 0; p < test.length; p++)
-		{
-			if (test[p])
-			{
-				temp = keys[p] + "%";
-				//System.out.println("Temp is -> " + temp + " and p is -> " + p + " and q is -> " + q);
-				
-				preparedStatement.setString(q, temp);
-				q++;
-			}
-		}
-		
-		//System.out.println("This is the selectSQL statement for the second time => " + selectSQL);
-		ResultSet rs = preparedStatement.executeQuery();
-
-		
-		
-		while (rs.next()) {
-            String titleTemp = rs.getString("TITLE").trim();
-            String authorTemp = rs.getString("AUTHOR").trim();
-            String bookNumb = rs.getString("ISBN13").trim();
-            String group = rs.getString("CATEGORY").trim();
-            
-            int count = rs.getInt("COPIES");
-            String describe = rs.getString("DESCRIPTION");
-            int year = rs.getInt("PublicationYear");
-            
-            
-            out.println("Book Title: " + titleTemp + ", ");
-	        out.println("Author: " + authorTemp + ", ");
-	        out.println("ISBN 13: " + bookNumb + ", ");
-	        out.println("Category: " + group + ", ");
-	        
-	        out.println("Copies: " + count + ", ");
-	        out.println("Description: " + describe + ", ");
-	        out.println("Year Published: " + year + "<br>");
-	        
-		}
-		out.println("<a href=/SemesterGroupProject/Book_Search.html>Search Data</a> <br>");
-        out.println("</body></html>");
-        rs.close();
-        preparedStatement.close();
-        connection.close();
-	}
-	
-	*/
-	
-	
-	
-	
-
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
