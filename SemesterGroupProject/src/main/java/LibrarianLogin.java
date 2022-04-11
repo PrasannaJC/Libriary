4b9f92163a6d554e1b297441e858e210732e6040
unoms <unoms@e523dfcf-a971-48cd-b7b2-28d4b80018ea>
2022-04-11 20:31:03 +0000

    changed and updated login method

--- SemesterGroupProject/src/main/java/LibrarianLogin.java
+++ SemesterGroupProject/src/main/java/LibrarianLogin.java
@@ -6,12 +6,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import util.LibrarianLoginUtil;
 /**
  * Servlet implementation class LibrarianLogin
  */
@@ -24,78 +26,101 @@ public class LibrarianLogin extends HttpServlet {
 	   }
 
 	   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		   String keyBranch = request.getParameter("BranchName");
+		   String keyBranch = request.getParameter("branchName");
 		      String keyPin = request.getParameter("PinCode");
-		      search(keyBranch, keyPin, response);
-
-	   }
-
-	   void search(String keyBranch, String keyPin, HttpServletResponse response) throws IOException {
-	     
-	      Connection connection = null;
-	      PreparedStatement preparedStatement = null;
-	      
-	      try {
-	         DBconnecter.getDBConnection(getServletContext());
-	         
-	         connection = DBconnecter.connection;
-	         
-	         if(!keyBranch.isEmpty() && !keyPin.isEmpty()) {
-	        	 String selectSQL = "SELECT * FROM  LibrarianTable WHERE BRANCH LIKE ? AND PIN LIKE ?";
-		            String branch = keyBranch + "%";
-		            String pin = keyPin + "%";
-		            preparedStatement = connection.prepareStatement(selectSQL);
-		            preparedStatement.setString(1, branch);
-		            preparedStatement.setString(2, pin);
-	         }
-	         
-	         ResultSet rs = preparedStatement.executeQuery();
-
-	         while (rs != null && rs.next()) {
-	            String branch = rs.getString("branch").trim();
-	            String pin = rs.getString("pin").trim();
-	            if(branch.contains(keyBranch) && pin.contains(keyPin)){
-	            	 response.setContentType("html");
-	       	      PrintWriter out = response.getWriter();
-	       	      out.println("/SemesterGroupProject/Home.html");
+		      boolean librarian = false;
+			  librarian = LibrarianLoginUtil.LibrarianLogin(keyBranch, keyPin);
+			  if (librarian == true) {
+				  response.setContentType("html");
+         	      PrintWriter out = response.getWriter();
+	       	      out.println("hello");
 	            }
-	            else if(!branch.contains(keyBranch) || !pin.contains(keyPin)){
-	            	 PrintWriter out = response.getWriter();
-		       	      out.println("incorrect branch or password");
-		       	      
-	            }
-	         }
-	         
-	         rs.close();
-	         preparedStatement.close();
-	         connection.close();
-	      
-	      } 
-	      catch (SQLException se) {
-	         se.printStackTrace();
-	      } 
-	      catch (Exception e) {
-	         e.printStackTrace();
-	      } 
-	      finally {
-	         try {
-	            if (preparedStatement != null)
-	               preparedStatement.close();
-	         } 
-	         catch (SQLException se2) {
-	         }
-	         try {
-	            if (connection != null)
-	               connection.close();
-	         } 
-	         catch (SQLException se) {
-	            se.printStackTrace();
-	         }
-	      }
+			  else {
+				  PrintWriter out = response.getWriter();
+	       	      out.println("incorrect branch or password");
+			  }
 	   }
 
 	   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	      doGet(request, response);
-	   }
+			// TODO Auto-generated method stub
+			doGet(request, response);
+		}
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+//	   void search(String keyBranch, String keyPin, HttpServletResponse response) throws IOException {
+//	     
+//	      Connection connection = null;
+//	      PreparedStatement preparedStatement = null;
+//	      
+//	      try {
+//	         DBconnecter.getDBConnection(getServletContext());
+//	         
+//	         connection = DBconnecter.connection;
+//	         
+//	         if(!keyBranch.isEmpty() && !keyPin.isEmpty()) {
+//	        	 String selectSQL = "SELECT * FROM  LibrarianLogin WHERE BRANCH LIKE ? AND PIN LIKE ?";
+//		            String branch = keyBranch + "%";
+//		            String pin = keyPin + "%";
+//		            preparedStatement = connection.prepareStatement(selectSQL);
+//		            preparedStatement.setString(1, branch);
+//		            preparedStatement.setString(2, pin);
+//	         }
+//	         
+//	         ResultSet rs = preparedStatement.executeQuery();
+//
+//	         while (rs != null && rs.next()) {
+//	            String branch = rs.getString("branch").trim();
+//	            String pin = rs.getString("pin").trim();
+//	            if(branch.contains(keyBranch) && pin.contains(keyPin)){
+//	            	 response.setContentType("html");
+//	       	      PrintWriter out = response.getWriter();
+//	       	      out.println("/SemesterGroupProject/Home.html");
+//	            }
+//	            else if(!branch.contains(keyBranch) || !pin.contains(keyPin)){
+//	            	 PrintWriter out = response.getWriter();
+//		       	      out.println("incorrect branch or password");
+//		       	      
+//	            }
+//	         }
+//	         
+//	         rs.close();
+//	         preparedStatement.close();
+//	         connection.close();
+//	      
+//	      } 
+//	      catch (SQLException se) {
+//	         se.printStackTrace();
+//	      } 
+//	      catch (Exception e) {
+//	         e.printStackTrace();
+//	      } 
+//	      finally {
+//	         try {
+//	            if (preparedStatement != null)
+//	               preparedStatement.close();
+//	         } 
+//	         catch (SQLException se2) {
+//	         }
+//	         try {
+//	            if (connection != null)
+//	               connection.close();
+//	         } 
+//	         catch (SQLException se) {
+//	            se.printStackTrace();
+//	         }
+//	      }
+//	   }
+//
+//	   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//	      doGet(request, response);
+//	   }
 
 }
