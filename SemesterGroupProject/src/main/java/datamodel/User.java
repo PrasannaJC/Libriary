071da7d8cea0aacba6d1596f0bce908a1c26007f
maiabennett <maiabennett@e523dfcf-a971-48cd-b7b2-28d4b80018ea>
2022-04-16 20:53:17 +0000

    Updated User datamodel to reflect new MySQL table params

--- SemesterGroupProject/src/main/java/datamodel/User.java
+++ SemesterGroupProject/src/main/java/datamodel/User.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.sql.Date;
 
 /**
  * @since J2SE-1.8
@@ -23,8 +24,17 @@ public class User {
    @Column(name = "USERID")
    private Integer userID;
 
+   @Column(name = "USERNAME")
+   private String username;
+   
    @Column(name = "BOOKS")
    private String books;
+   
+   @Column(name = "CHECKOUT")
+   private java.sql.Date checkout;
+   
+   @Column(name = "DUE")
+   private java.sql.Date due;
 
    @Column(name = "OVERDUE")
    private String overdue;
@@ -32,21 +42,15 @@ public class User {
    public User() {
    }
 
-   public User(Integer userID, String books, String overdue) {
-      this.userID = userID;
-      this.books = books;
-      this.overdue = overdue;
-   }
-
-   /*
-    * I am including this to be used as a librarian insert of userID, book ISBN13
-    * UtilDB will need to have a function to set overdue to False with timer going until true 
-    * I envision this will be something along the lines of [current date] + [2 weeks?] set to '' (true)
-    */
+   // for an existing user, need conversion to java.sql.Date and due date calculation to happen in the Util; need to UPDATE not create user
+   // as such, there is no create user command with the ID.
    
-   public User(Integer userID, String books) {
-	  this.userID = userID;
+   // for a new user, need conversion to java.sql.Date and due date calculation to happen in the Util
+   public User(String username, String books, java.sql.Date checkout, java.sql.Date due) {
+	  this.username = username;
 	  this.books = books;
+	  this.checkout = checkout;
+	  this.due = due;
    }
   
 
@@ -58,12 +62,36 @@ public class User {
       this.userID = userID;
    }
 
+   public String getUsername() {
+	   return username;
+   }
+   
+   public void setUsername(String username) {
+	   this.username = username;
+   }
+   
    public String getBooks() {
       return books;
    }
 
    public void setBooks(String books) {
       this.books = books;
+   }
+   
+   public java.sql.Date getCheckout() {
+	   return checkout;
+   }
+   
+   public void setCheckout(java.sql.Date checkout) {
+	   this.checkout = checkout;
+   }
+   
+   public java.sql.Date getDue() {
+	   return due;
+   }
+   
+   public void setDue(java.sql.Date due) {
+	   this.due = due;
    }
 
    public String getOverdue() {
@@ -76,6 +104,6 @@ public class User {
    
    @Override
    public String toString() {
-      return "Book: " + this.userID + ", " + this.books + ", " + this.overdue;
+      return "User: " + this.userID + ", " + this.username + ", " + this.books + ", " + this.checkout + ", " + this.due + ", " + this.overdue;
    }
 }
