22b09a30a03d3665b54131722267d6360db5fa2e
pjstudent <pjstudent@e523dfcf-a971-48cd-b7b2-28d4b80018ea>
2022-04-17 02:48:25 +0000

    Made some progress (not as much as I'd like) on the User search and set up a way to check if a user even exist's before the checkout process - also accounted for the #copies for a book but haven't added in the part to update the table to reflect the book being checked out.

--- SemesterGroupProject/src/main/java/util/UserUtil.java
+++ SemesterGroupProject/src/main/java/util/UserUtil.java
@@ -1,12 +1,16 @@
 package util;
 
 import java.util.List;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Iterator;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+
 
 import datamodel.User;
 
@@ -42,8 +46,8 @@ public class UserUtil {
 
 	      try {
 	         tx = session.beginTransaction();
-	         List<?> bks = session.createQuery("FROM User").list();
-	         for (Iterator<?> iterator = bks.iterator(); iterator.hasNext();) 
+	         List<?> usr = session.createQuery("FROM User").list();
+	         for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
 	         {
 	            User u = (User) iterator.next();
 	            resultList.add(u);
@@ -59,8 +63,96 @@ public class UserUtil {
 	      return resultList;
 	}	
 	
+	public static List<User> listUsers(Integer userID, String userName, String[] books) 
+	{
+	      List<User> resultList = new ArrayList<User>();
+
+	      Session session = getSessionFactory().openSession();
+	      Transaction tx = null;
+
+	      try {
+	         tx = session.beginTransaction();
+	         List<?> usr = session.createQuery("FROM User").list();
+	         for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
+	         {
+	            User u = (User) iterator.next();
+	         /*   if ((userID == null || u.getUserID() == userID) && (books == null || u.getBooks().equals(books)) && (overdue == "" || u.getUserID() == userID))
+	            {
+	            	
+	            }
+	            
+	            */
+	            
+	            
+	            resultList.add(u);
+	         }
+	         tx.commit();
+	      } catch (HibernateException e) {
+	         if (tx != null)
+	            tx.rollback();
+	         e.printStackTrace();
+	      } finally {
+	         session.close();
+	      }
+	      return resultList;
+	}
 	
 	
+	public static boolean checkUser(Integer userID)
+	{
+		//List<User> resultList = new ArrayList<User>();
+
+	      Session session = getSessionFactory().openSession();
+	      Transaction tx = null;
+	      Boolean out = false;
+
+	      try {
+	         tx = session.beginTransaction();
+	         List<?> usr = session.createQuery("FROM User").list();
+	         for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
+	         {
+	        	 User u = (User) iterator.next();
+	        	 if (u.getUserID().equals(userID))
+	        	 {
+	        		 out = true;
+	        		 break;
+	        	 }
+	        	 else
+	        	 {
+	        		 out = false;
+	        	 }
+	         }
+	      } catch (HibernateException e) {
+	         if (tx != null)
+	            tx.rollback();
+	         e.printStackTrace();
+	      } finally {
+	         session.close();
+	      }
+	         return out;
+	}
+	
+	public static void createUser(String userName, String books, java.sql.Date checkout) {
+	      Session session = getSessionFactory().openSession();
+	      Transaction tx = null;
+	      try {
+	         tx = session.beginTransaction();
+	         //java.util.Date due = new java.util.Date(checkout.getTime());
+	         LocalDate start = checkout.toLocalDate();
+	         LocalDate due = start.plusDays(14);
+	         java.sql.Date d = Date.valueOf(due);
+
+	         session.save(new User(userName, books, checkout, d));
+	         
+	         tx.commit();
+	      } catch (HibernateException e) {
+	         if (tx != null)
+	            tx.rollback();
+	         e.printStackTrace();
+	      } finally {
+	         session.close();
+	      }
+	   }
 	
 	
 	
