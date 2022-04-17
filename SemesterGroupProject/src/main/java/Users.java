75fd4ede92eaf4a1ef1d28d4b9a9fc4d9398a07a
pjstudent <93747727+PrasannaJC@users.noreply.github.com>
2022-04-17 02:48:25 +0000

    Made some progress (not as much as I'd like) on the User search and set up a way to check if a user even exist's before the checkout process - also accounted for the #copies for a book but haven't added in the part to update the table to reflect the book being checked out.

--- SemesterGroupProject/src/main/java/Users.java
+++ SemesterGroupProject/src/main/java/Users.java
@@ -1,11 +1,15 @@
-
-
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import datamodel.Book;
+import util.BookUtil;
+import util.UserUtil;
 
 /**
  * Servlet implementation class Users
@@ -14,27 +18,87 @@ import javax.servlet.http.HttpServletResponse;
 public class Users extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Users() {
+
+	
+	
+	public Users() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String keyID = request.getParameter("userID"); //label it as whatever is designated in the HTML
+		String keyName = request.getParameter("userName"); //label it as whatever is designated in the HTML
+		String keyISBN = request.getParameter("bookList"); //label it as whatever is designated in the HTML
+		
+		String[] bookList = keyISBN.split(", ");
+		/*try {
+			//bookList.length() <= 5;
+			int k = bookList.length;
+		}
+		catch (IndexOutOfBoundsException e){
+			
+		}*/
+		
+		if (userExist(Integer.getInteger(keyID)))
+		{
+			userCheckout(bookList);
+		}
+		
+		/*try {
+			
+			
+		}
+		catch (NullPointerException e){
+			bookList = null;
+		}
+		*/
+		
+		
+		
+	}
+	
+	public Boolean userExist(Integer keyID)
+	{
+		//UserUtil.checkUser(keyID);
+		
+		if (UserUtil.checkUser(keyID))
+		{
+			System.out.println("<li> The following user " + keyID + " exists and can check out books!!!");
+			return UserUtil.checkUser(keyID);
+		}
+		else
+		{
+			System.out.println("<li> The following user " + keyID + " does not exists, so go create a new user");
+			return false;
+		}
+	}
+	
+	/**
+	 * This is the user checkout starting point, where I create a list of the books that are available at the library and still have copies on shelves.
+	 * bks is the list of books that meet the above condition.
+	 * based on that, 
+	 * @param bookList
+	 */
+	public void userCheckout(String[] bookList)
+	{
+		List<Book> bkz = null;
+		List<Book> bks = null;
+		for (int i = 0; i < bookList.length; i++)
+		{
+			for (int j = 1; j < 6; j++)
+			{
+				bkz = BookUtil.listBooks(null, null, bookList[i], "Please", j, 10000);
+				bks.addAll(bkz);
+			}		
+		}
+		
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
