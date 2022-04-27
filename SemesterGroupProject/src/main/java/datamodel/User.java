0cb1c641ff97a528b6713a322a8d670a711000ba
shellis0 <shellis0@e523dfcf-a971-48cd-b7b2-28d4b80018ea>
2022-04-27 01:19:24 +0000

    Cleaned up the organization of the data models and removed any unused import statements for the final project demo.

--- SemesterGroupProject/src/main/java/datamodel/User.java
+++ SemesterGroupProject/src/main/java/datamodel/User.java
@@ -5,7 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.sql.Date;
 
 /**
  * @since J2SE-1.8
@@ -17,93 +16,93 @@ import java.sql.Date;
  */
 @Entity
 @Table(name = "UserTable")
-public class User {
+public class User
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "USERID")
+	private Integer userID;
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   @Column(name = "USERID")
-   private Integer userID;
-
-   @Column(name = "USERNAME")
-   private String username;
+	@Column(name = "USERNAME")
+	private String username;
    
-   @Column(name = "BOOKS")
-   private String books;
+	@Column(name = "BOOKS")
+	private String books;
    
-   @Column(name = "CHECKOUT")
-   private java.sql.Date checkout;
+	@Column(name = "CHECKOUT")
+	private java.sql.Date checkout;
    
-   @Column(name = "DUE")
-   private java.sql.Date due;
-
-   @Column(name = "OVERDUE")
-   private String overdue;
+	@Column(name = "DUE")
+	private java.sql.Date due;
       
-   public User() {
-   }
+	public User()
+	{}
 
-   // for an existing user, need conversion to java.sql.Date and due date calculation to happen in the Util; need to UPDATE not create user
-   // as such, there is no create user command with the ID.
+	// for an existing user, need conversion to java.sql.Date and due date calculation to happen in the Util; need to UPDATE not create user
+	// as such, there is no create user command with the ID.
    
-   // for a new user, need conversion to java.sql.Date and due date calculation to happen in the Util
-   public User(String username, String books, java.sql.Date checkout, java.sql.Date due) {
-	  this.username = username;
-	  this.books = books;
-	  this.checkout = checkout;
-	  this.due = due;
-   }
+	// for a new user, need conversion to java.sql.Date and due date calculation to happen in the Util
+	public User(String username, String books, java.sql.Date checkout, java.sql.Date due)
+	{
+		this.username = username;
+		this.books = books;
+		this.checkout = checkout;
+		this.due = due;
+	}
   
+	public Integer getUserID()
+	{
+		return userID;
+	}
 
-   public Integer getUserID() {
-      return userID;
-   }
+	public void setUserID(Integer userID)
+	{
+		this.userID = userID;
+	}
 
-   public void setUserID(Integer userID) {
-      this.userID = userID;
-   }
+	public String getUsername()
+	{
+	   	return username;
+	}
+   
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+   
+	public String getBooks()
+	{
+		return books;
+	}
 
-   public String getUsername() {
-	   return username;
-   }
+	public void setBooks(String books)
+	{
+		this.books = books;
+	}
    
-   public void setUsername(String username) {
-	   this.username = username;
-   }
+	public java.sql.Date getCheckout()
+	{
+		return checkout;
+	}
    
-   public String getBooks() {
-      return books;
-   }
-
-   public void setBooks(String books) {
-      this.books = books;
-   }
+	public void setCheckout(java.sql.Date checkout)
+	{
+		this.checkout = checkout;
+	}
    
-   public java.sql.Date getCheckout() {
-	   return checkout;
-   }
+	public java.sql.Date getDue()
+	{
+		return due;
+	}
    
-   public void setCheckout(java.sql.Date checkout) {
-	   this.checkout = checkout;
-   }
+	public void setDue(java.sql.Date due)
+	{
+		this.due = due;
+	}
    
-   public java.sql.Date getDue() {
-	   return due;
-   }
-   
-   public void setDue(java.sql.Date due) {
-	   this.due = due;
-   }
-
-   public String getOverdue() {
-      return overdue;
-   }
-
-   public void setOverdue(String overdue) {
-      this.overdue = overdue;
-   }
-   
-   @Override
-   public String toString() {
-      return "User: " + this.userID + ", " + this.username + ", " + this.books + ", " + this.checkout + ", " + this.due + ", " + this.overdue;
-   }
+	@Override
+   	public String toString()
+	{
+		return "User: " + this.userID + ", " + this.username + ", " + this.books + ", " + this.checkout + ", " + this.due;
+	}
 }
