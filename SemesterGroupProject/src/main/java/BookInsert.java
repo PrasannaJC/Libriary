7461d0281a996ead112dd26007aecc062a22c3e2
shellis0 <shellis0@e523dfcf-a971-48cd-b7b2-28d4b80018ea>
2022-04-27 03:15:26 +0000

    Cleaned the function classes for the the final project demo.

--- SemesterGroupProject/src/main/java/BookInsert.java
+++ SemesterGroupProject/src/main/java/BookInsert.java
@@ -15,72 +15,72 @@ import util.Info;
  * Servlet implementation class BookInsert
  */
 @WebServlet("/BookInsert")
-public class BookInsert extends HttpServlet {
+public class BookInsert extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BookInsert() {
-        super();
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+    public BookInsert()
+    {
+    	super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String keyTitle = request.getParameter("booktitle"); //label it as whatever is designated in the HTML
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+    	String keyTitle = request.getParameter("booktitle"); //label it as whatever is designated in the HTML
 		String keyAuthor = request.getParameter("writer"); //label it as whatever is designated in the HTML
 		String keyISBN = request.getParameter("ISBN"); //label it as whatever is designated in the HTML
 		String keyCategory = request.getParameter("category"); //label it as whatever is designated in the HTML
 		Integer keyCopy = Integer.valueOf(request.getParameter("copies")); //label it as whatever is designated in the HTML
-		//String keyDescription = request.getParameter("description"); 
-		Integer keyYear = Integer.valueOf(request.getParameter("date")); //label it as whatever is designated in the HTML// 
+		Integer keyYear = Integer.valueOf(request.getParameter("date")); //label it as whatever is designated in the HTML 
 		
 		response.setContentType("text/html");
 	    PrintWriter out = response.getWriter();
 		
-		if (keyTitle == null || keyAuthor == null || keyISBN == null
-				|| keyCategory.equals("Please") || keyCopy.equals(999) || keyYear.equals(10000))
-		{
-			RequestDispatcher rd=request.getRequestDispatcher("Book_Insert.html");  
-	        rd.include(request,response);
-	        out.println("<script type=\"text/javascript\">");
-			out.println("alert('Missing Parameters Detected, Please Make Sure all Parameters are Filled or Selected');");
-			out.println("</script>");
-		}
-		else
-		{	
-			BookUtil.createBook(keyTitle, keyAuthor, keyISBN, keyCategory, keyCopy, keyYear);
+	    if (keyTitle == null || keyAuthor == null || keyISBN == null
+	    		|| keyCategory.equals("Please") || keyCopy.equals(999) || keyYear.equals(10000))
+	    {
+	    	RequestDispatcher rd=request.getRequestDispatcher("Book_Insert.html");  
+	    	rd.include(request,response);
+	    	out.println("<script type=\"text/javascript\">");
+	    	out.println("alert('Missing Parameters Detected, Please Make Sure all Parameters are Filled or Selected');");
+	    	out.println("</script>");
+	    }
+	    else
+	    {	
+	    	BookUtil.createBook(keyTitle, keyAuthor, keyISBN, keyCategory, keyCopy, keyYear);
 			
-		      //response.setContentType("text/html");
-		      //PrintWriter out = response.getWriter();
-		      String title = "Book Inserted!";
-		      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n"; //
-		      out.println(docType + //
-		            "<html>\n" + //
+		    String title = "Book Inserted!";
+		    String docType = "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n"; //
+		    out.println(docType + //
+		    		"<html>\n" + //
 		            "<head><title>" + title + "</title></head>\n" + //
-		            "<body bgcolor=\"#f0f0f0\">\n" + //
+		            "<body bgcolor=\"#b5a965\">\n" + //
 		            "<h1 align=\"center\">" + title + "</h1>\n");
-		      out.println("<ul>");
-		      out.println("<li> Book Title: " + keyTitle);
-		      out.println("<li> Author: " + keyAuthor);
-		      out.println("<li> ISBN: " + keyISBN);
-		      out.println("<li> Category " + keyCategory);
-		      out.println("<li> Number of Copies: " + keyCopy);
-		      out.println("<li> Publication Year: " + keyYear);
-		      out.println("</ul>");
-		      out.println("<a href=/" + Info.projectName + "/" + Info.searchWebName + ">Insert Another Book</a> <br>");
-		      out.println("<a href=/" + Info.projectName + "/" + Info.homeWebName + ">Back to Home Page</a> <br>");
-		      out.println("</body></html>");
-		}
-	}
+		    out.println("<ul>");
+		    out.println("<li> Book Title: " + keyTitle);
+		    out.println("<li> Author: " + keyAuthor);
+		    out.println("<li> ISBN: " + keyISBN);
+		    out.println("<li> Category " + keyCategory);
+		    out.println("<li> Number of Copies: " + keyCopy);
+		    out.println("<li> Publication Year: " + keyYear);
+		    out.println("</ul>");
+		    out.println("<a href=/" + Info.projectName + "/" + Info.searchWebName + ">Insert Another Book</a> <br>");
+		    out.println("<a href=/" + Info.projectName + "/" + Info.homeWebName + ">Back to Home Page</a> <br>");
+		    out.println("</body></html>");
+	    }
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
-
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+    	doGet(request, response);
+    }
 }
--- SemesterGroupProject/src/main/java/LibrarianLogin.java
+++ SemesterGroupProject/src/main/java/LibrarianLogin.java
@@ -1,12 +1,6 @@
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -15,122 +9,49 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.tomcat.jni.File;
-
 import util.LibrarianLoginUtil;
+
 /**
- * Servlet implementation class LibrarianLogin
- */
+* Servlet implementation class LibrarianLogin
+*/
 @WebServlet("/LibrarianLogin")
-public class LibrarianLogin extends HttpServlet {
+public class LibrarianLogin extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
 
-	   public LibrarianLogin() {
-	      super();
-	   }
+	public LibrarianLogin()
+	{
+		super();
+	}
 
-	   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		   response.setContentType("text/html");  
-		   PrintWriter out = response.getWriter();  
-		   String keyBranch = request.getParameter("branchName");
-		      String keyPin = request.getParameter("pinCode");
-		      boolean librarian = false;
-			  librarian = LibrarianLoginUtil.LibrarianLogin(keyBranch, keyPin);
-			  if (librarian) {
-				  RequestDispatcher rd=request.getRequestDispatcher("Home.html");  
-			        rd.forward(request,response);  
-			  }
-			  else {
-				  //out.print("Sorry username or password error");
-			        RequestDispatcher rd=request.getRequestDispatcher("LoginPage.html");  
-			        rd.include(request,response);
-			        out.println("<script type=\"text/javascript\">");
-					out.println("alert('The Branch Name or Pin Code is Invalid');");
-					out.println("</script>");
-			  }
-			  out.close();  
-	   }
-
-	   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			// TODO Auto-generated method stub
-			doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		response.setContentType("text/html");  
+		PrintWriter out = response.getWriter();  
+		String keyBranch = request.getParameter("branchName");
+		String keyPin = request.getParameter("pinCode");
+		boolean librarian = false;
+		librarian = LibrarianLoginUtil.LibrarianLogin(keyBranch, keyPin);
+		
+		if (librarian)
+		{
+			RequestDispatcher rd = request.getRequestDispatcher("Home.html");  
+				rd.forward(request,response);  
 		}
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-//	   void search(String keyBranch, String keyPin, HttpServletResponse response) throws IOException {
-//	     
-//	      Connection connection = null;
-//	      PreparedStatement preparedStatement = null;
-//	      
-//	      try {
-//	         DBconnecter.getDBConnection(getServletContext());
-//	         
-//	         connection = DBconnecter.connection;
-//	         
-//	         if(!keyBranch.isEmpty() && !keyPin.isEmpty()) {
-//	        	 String selectSQL = "SELECT * FROM  LibrarianLogin WHERE BRANCH LIKE ? AND PIN LIKE ?";
-//		            String branch = keyBranch + "%";
-//		            String pin = keyPin + "%";
-//		            preparedStatement = connection.prepareStatement(selectSQL);
-//		            preparedStatement.setString(1, branch);
-//		            preparedStatement.setString(2, pin);
-//	         }
-//	         
-//	         ResultSet rs = preparedStatement.executeQuery();
-//
-//	         while (rs != null && rs.next()) {
-//	            String branch = rs.getString("branch").trim();
-//	            String pin = rs.getString("pin").trim();
-//	            if(branch.contains(keyBranch) && pin.contains(keyPin)){
-//	            	 response.setContentType("html");
-//	       	      PrintWriter out = response.getWriter();
-//	       	      out.println("/SemesterGroupProject/Home.html");
-//	            }
-//	            else if(!branch.contains(keyBranch) || !pin.contains(keyPin)){
-//	            	 PrintWriter out = response.getWriter();
-//		       	      out.println("incorrect branch or password");
-//		       	      
-//	            }
-//	         }
-//	         
-//	         rs.close();
-//	         preparedStatement.close();
-//	         connection.close();
-//	      
-//	      } 
-//	      catch (SQLException se) {
-//	         se.printStackTrace();
-//	      } 
-//	      catch (Exception e) {
-//	         e.printStackTrace();
-//	      } 
-//	      finally {
-//	         try {
-//	            if (preparedStatement != null)
-//	               preparedStatement.close();
-//	         } 
-//	         catch (SQLException se2) {
-//	         }
-//	         try {
-//	            if (connection != null)
-//	               connection.close();
-//	         } 
-//	         catch (SQLException se) {
-//	            se.printStackTrace();
-//	         }
-//	      }
-//	   }
-//
-//	   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//	      doGet(request, response);
-//	   }
+		else
+		{
+			RequestDispatcher rd = request.getRequestDispatcher("LoginPage.html");  
+			rd.include(request,response);
+			out.println("<script type=\"text/javascript\">");
+			out.println("alert('The Branch Name or Pin Code is Invalid');");
+			out.println("</script>");
+		}
+		
+		out.close();  
+	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		doGet(request, response);
+	}
 }
