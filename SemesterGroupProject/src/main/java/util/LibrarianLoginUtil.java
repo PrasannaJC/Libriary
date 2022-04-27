5175e343aac65357f4c042d2b95d44a58f3f722b
shellis0 <shellis0@e523dfcf-a971-48cd-b7b2-28d4b80018ea>
2022-04-27 01:53:07 +0000

    Cleaned up the utility files leading up to the final project demo.

--- SemesterGroupProject/src/main/java/util/LibrarianLoginUtil.java
+++ SemesterGroupProject/src/main/java/util/LibrarianLoginUtil.java
@@ -17,77 +17,113 @@ import org.hibernate.Transaction;
 /**
  * @since JavaSE-1.8
  */
-public class LibrarianLoginUtil {
+public class LibrarianLoginUtil
+{
 	static SessionFactory sessionFactory = null;
 
-	   public static SessionFactory getSessionFactory() {
-	      if (sessionFactory != null) {
-	         return sessionFactory;
-	      }
-	      Configuration configuration = new Configuration().configure();
-	      StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-	      sessionFactory = configuration.buildSessionFactory(builder.build());
-	      return sessionFactory;
-	   }
-	   public static Boolean LibrarianLogin(String keyBranch, String keyPin) {
-		   Session session = getSessionFactory().openSession();
-		   Transaction tx = null;
+	public static SessionFactory getSessionFactory()
+	{
+		if (sessionFactory != null)
+		{
+			return sessionFactory;
+		}
+	    
+		Configuration configuration = new Configuration().configure();
+	    StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+	    sessionFactory = configuration.buildSessionFactory(builder.build());
+	    return sessionFactory;
+	}
+	   
+	public static Boolean LibrarianLogin(String keyBranch, String keyPin)
+	{
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
 
-		   try {
-			   tx = session.beginTransaction();
-			   List<?> LL = session.createQuery("FROM LibrarianTable").list();
-			   for (Iterator<?> iterator = LL.iterator(); iterator.hasNext();) {
-				   LibrarianTable L = (LibrarianTable) iterator.next();
-				   if (L.getbranch().equals(keyBranch) &&  L.getPin().equals(keyPin)) {
-					   		return true;
-		           }				   
-			   }
-			   tx.commit();
-	      } catch (HibernateException e) {
+		try
+		{
+			tx = session.beginTransaction();
+			List<?> LL = session.createQuery("FROM LibrarianTable").list();
+			
+			for (Iterator<?> iterator = LL.iterator(); iterator.hasNext();)
+			{
+				LibrarianTable L = (LibrarianTable) iterator.next();
+				
+				if (L.getbranch().equals(keyBranch) &&  L.getPin().equals(keyPin))
+				{
+					return true;
+				}				   
+			}
+			
+			tx.commit();
+		}
+		catch (HibernateException e)
+		{
 	         if (tx != null)
 	            tx.rollback();
 	         e.printStackTrace();
-	      } finally {
+		}
+		finally
+		{
 	         session.close();
-	      }
-	      return false;
-	   }
-	   public static void createlibrarian(String branchName, String pinName) {
-		      Session session = getSessionFactory().openSession();
-		      Transaction tx = null;
-		      try {
-		         tx = session.beginTransaction();
-		         session.save(new LibrarianTable(branchName, pinName));
-		         tx.commit();
-		      } catch (HibernateException e) {
-		         if (tx != null)
-		            tx.rollback();
-		         e.printStackTrace();
-		      } finally {
-		         session.close();
-		      }
-		   }
-	   public static List<LibrarianTable> listLibrarian() {
-		      List<LibrarianTable> resultList = new ArrayList<LibrarianTable>();
+		}
+		
+		return false;
+	}
+	
+	public static void createlibrarian(String branchName, String pinName)
+	{
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
+		
+		try
+		{
+			tx = session.beginTransaction();
+			session.save(new LibrarianTable(branchName, pinName));
+			tx.commit();
+		}
+		catch (HibernateException e)
+		{
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+		}
+	}
+	
+	public static List<LibrarianTable> listLibrarian()
+	{
+		List<LibrarianTable> resultList = new ArrayList<LibrarianTable>();
 
-		      Session session = getSessionFactory().openSession();
-		      Transaction tx = null;
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
 
-		      try {
-		         tx = session.beginTransaction();
-		         List<?> rsvps = session.createQuery("FROM LibrarianTable").list();
-		         for (Iterator<?> iterator = rsvps.iterator(); iterator.hasNext();) {
-		            LibrarianTable rsvpIt = (LibrarianTable) iterator.next();
-		            resultList.add(rsvpIt);
-		         }
-		         tx.commit();
-		      } catch (HibernateException e) {
-		         if (tx != null)
-		            tx.rollback();
-		         e.printStackTrace();
-		      } finally {
-		         session.close();
-		      }
-		      return resultList;
-		   }
+		try
+		{
+			tx = session.beginTransaction();
+		    List<?> rsvps = session.createQuery("FROM LibrarianTable").list();
+		    
+		    for (Iterator<?> iterator = rsvps.iterator(); iterator.hasNext();)
+		    {
+		    	LibrarianTable rsvpIt = (LibrarianTable) iterator.next();
+		        resultList.add(rsvpIt);
+		    }
+		    
+		    tx.commit();
+		}
+		catch (HibernateException e)
+		{
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+		}
+		
+		return resultList;
+	}
 }
--- SemesterGroupProject/src/main/java/util/UserUtil.java
+++ SemesterGroupProject/src/main/java/util/UserUtil.java
@@ -19,8 +19,8 @@ import org.hibernate.Transaction;
 /**
  * @since JavaSE-1.8
  */
-public class UserUtil {
-	
+public class UserUtil
+{
 	static SessionFactory sessionFactory = null;
 
 	public static SessionFactory getSessionFactory() 
@@ -29,6 +29,7 @@ public class UserUtil {
 		{
 			return sessionFactory;
 		}
+		
 		Configuration configuration = new Configuration().configure();
 		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
 		sessionFactory = configuration.buildSessionFactory(builder.build());
@@ -37,247 +38,311 @@ public class UserUtil {
 	
 	public static List<User> listUsers() 
 	{
-	      List<User> resultList = new ArrayList<User>();
+		List<User> resultList = new ArrayList<User>();
 
-	      Session session = getSessionFactory().openSession();
-	      Transaction tx = null;
+	    Session session = getSessionFactory().openSession();
+	    Transaction tx = null;
 
-	      try {
-	         tx = session.beginTransaction();
-	         List<?> usr = session.createQuery("FROM User").list();
-	         for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
-	         {
-	            User u = (User) iterator.next();
-	            resultList.add(u);
-	         }
-	         tx.commit();
-	      } catch (HibernateException e) {
+	    try
+	    {
+	    	tx = session.beginTransaction();
+	        List<?> usr = session.createQuery("FROM User").list();
+	        
+	        for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
+	        {
+	        	User u = (User) iterator.next();
+	        	resultList.add(u);
+	        }
+	        tx.commit();
+	    }
+	    catch (HibernateException e)
+	    {
 	         if (tx != null)
 	            tx.rollback();
 	         e.printStackTrace();
-	      } finally {
-	         session.close();
-	      }
-	      return resultList;
+	    }
+	    finally
+	    {
+	    	session.close();
+	    }
+	    
+	    return resultList;
 	}	
 	
 	public static List<User> listUsers(Integer userID, String userName, String books) 
 	{
-	      List<User> resultList = new ArrayList<User>();
+		List<User> resultList = new ArrayList<User>();
 
-	      Session session = getSessionFactory().openSession();
-	      Transaction tx = null;
+	    Session session = getSessionFactory().openSession();
+	    Transaction tx = null;
 
-			System.out.println("=> " + userID);
-			System.out.println("=> " + userName);
-			System.out.println("=> " + books);
+		System.out.println("=> " + userID);
+		System.out.println("=> " + userName);
+		System.out.println("=> " + books);
 			
-	      try {
-	         tx = session.beginTransaction();
-	         List<?> usr = session.createQuery("FROM User").list();
-	         for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
-	         {
+	    try
+	    {
+	    	tx = session.beginTransaction();
+	        List<?> usr = session.createQuery("FROM User").list();
+	        
+	        for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
+	        { 
 	            User u = (User) iterator.next();
-	            if ((userID == null || u.getUserID().equals(userID)) && (books == "" || books == null || u.getBooks().contains(books)) && (userName == "" || userName == null || u.getUsername().startsWith(userName)))
+	            String tempBook;
+	            
+	            if (u.getBooks() == null)
+	            {
+	            	tempBook = "";
+	            }
+	            else
+	            {
+	            	tempBook = u.getBooks();
+	            }	
+	            
+	            if ((userID == null || u.getUserID().equals(userID))
+	            		&& (books == null || books.equals("") || tempBook.contains(books))
+	            		&& (userName == "" || userName == null || u.getUsername().startsWith(userName)))
 	            {
 	            	resultList.add(u);
 	            }
-
-	         }
-	         tx.commit();
-	      } catch (HibernateException e) {
-	         if (tx != null)
-	            tx.rollback();
-	         e.printStackTrace();
-	      } finally {
-	         session.close();
-	      }
-	      return resultList;
+	        }
+	        tx.commit();
+	    }
+	    catch (HibernateException e)
+	    {
+	    	if (tx != null)
+	    		tx.rollback();
+	    	e.printStackTrace();
+	    }
+	    finally
+	    {
+	    	session.close();
+	    }
+	    
+	    return resultList;
 	}
-	
 	
 	public static boolean checkUser(String userID)
 	{
-	      Session session = getSessionFactory().openSession();
-	      Transaction tx = null;
-	      Boolean out = false;
-	      int x = Integer.parseInt(userID); 
-	      try {
-	         tx = session.beginTransaction();
-	         List<?> usr = session.createQuery("FROM User").list();
-	         for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
-	         {
-	        	 User u = (User) iterator.next();
-	        	 if (u.getUserID() == x)
-	        	 {
-	        		 out = true;
-	        		 break;
-	        	 }
-	        	 else
-	        	 {
-	        		 out = false;
-	        	 }
-	         }
-	      } catch (HibernateException e) {
+		Session session = getSessionFactory().openSession();
+	    Transaction tx = null;
+	    Boolean out = false;
+	    int x = Integer.parseInt(userID); 
+	    
+	    try
+	    {
+	    	tx = session.beginTransaction();
+	        List<?> usr = session.createQuery("FROM User").list();
+	        
+	        for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
+	        {
+	        	User u = (User) iterator.next();
+	        	if (u.getUserID() == x)
+	        	{
+	        		out = true;
+	        		break;
+	        	}
+	        	else
+	        	{
+	        		out = false;
+	        	}
+	        }
+	    }
+	    catch (HibernateException e)
+	    {
 	         if (tx != null)
 	            tx.rollback();
 	         e.printStackTrace();
-	      } finally {
-	         session.close();
-	      }
-	         return out;
+	    }
+	    finally
+	    {
+	    	session.close();
+	    }
+	    
+	    return out;
 	}
 	
-	public static void createUser(String userName, String books) {
-	      Session session = getSessionFactory().openSession();
-	      Transaction tx = null;
-	      try {
-	         tx = session.beginTransaction();
-			//java.util.Date due = new java.util.Date(checkout.getTime());
-	         LocalDate start = LocalDate.now();
-	         LocalDate due = start.plusDays(14);
-	         java.sql.Date d = Date.valueOf(due);
-	         java.sql.Date c = Date.valueOf(start);
-	         session.save(new User(userName, books, c, d));
+	public static void createUser(String userName, String books)
+	{
+		Session session = getSessionFactory().openSession();
+	    Transaction tx = null;
+	    
+	    try
+	    {
+	    	tx = session.beginTransaction();
+
+	        LocalDate start = LocalDate.now();
+	        LocalDate due = start.plusDays(14);
+	        java.sql.Date d = Date.valueOf(due);
+	        java.sql.Date c = Date.valueOf(start);
+	        session.save(new User(userName, books, c, d));
 	         
 	         tx.commit();
-	      } catch (HibernateException e) {
+	    }
+	    catch (HibernateException e)
+	    {
 	         if (tx != null)
 	            tx.rollback();
 	         e.printStackTrace();
-	      } finally {
+	    }
+	    finally
+	    {
 	         session.close();
-	      }
-	   }
+	    }
+	}
 	
-	public static boolean updateUser(Integer UserID, String books) {
+	public static boolean updateUser(Integer UserID, String books)
+	{
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
 
-        Session session = getSessionFactory().openSession();
-          Transaction tx = null;
-
-          try {
-             tx = session.beginTransaction();
-             List<?> usr = session.createQuery("FROM User").list();
-             for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
-             {
-                 User u = (User) iterator.next();
-                 if (u.getUserID().equals(UserID) && u.getDue() == null)
-                 {
-                	 	u.setBooks(books);
-                	 	LocalDate checkout = LocalDate.now();
-                        LocalDate due = checkout.plusDays(14);
-                        java.sql.Date d = Date.valueOf(due);
-                        java.sql.Date c = Date.valueOf(checkout);
-                        u.setCheckout(c);
-                        u.setDue(d);
-                        session.update(u);
+		try
+		{
+			tx = session.beginTransaction();
+            List<?> usr = session.createQuery("FROM User").list();
+            
+            for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
+            {
+            	User u = (User) iterator.next();
+                if (u.getUserID().equals(UserID) && u.getDue() == null)
+                {
+                	u.setBooks(books);
+                	LocalDate checkout = LocalDate.now();
+                    LocalDate due = checkout.plusDays(14);
+                    java.sql.Date d = Date.valueOf(due);
+                    java.sql.Date c = Date.valueOf(checkout);
+                    u.setCheckout(c);
+                    u.setDue(d);
+                    session.update(u);
                        
-                        return true;
-                 }
-             }
-          } catch (HibernateException e) {
-             if (tx != null)
-                tx.rollback();
-             e.printStackTrace();
-          } finally {
-        	 tx.commit();
-             session.close();
-          }
-          return false;
-       }
-	public static void returnUserBook(Integer UserID, String book) {
+                    return true;
+                }
+            }
+		}
+		catch (HibernateException e)
+		{
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		}
+		finally
+		{
+			tx.commit();
+            session.close();
+		}
+        
+		return false;
+	}
+	
+	public static void returnUserBook(Integer UserID, String book)
+	{
+		Session session = getSessionFactory().openSession();
+        Transaction tx = null;
 
-        Session session = getSessionFactory().openSession();
-          Transaction tx = null;
-
-          try {
-             tx = session.beginTransaction();
-             List<?> usr = session.createQuery("FROM User").list();
-             for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
-             {
-                 User u = (User) iterator.next();
-                 if (u.getUserID().equals(UserID)){
-                	 System.out.println(u.getBooks());
-                	 String bookin[] = u.getBooks().split(", ");
-                     String books = "";
-		             for (String bookpos : bookin) {
-		                 if (bookpos.equals(book)){
-		                        bookpos = "";
-		                 } 
-		                 if (bookpos.equals("") && books.equals("")) {
-		                	 books = bookpos;
-		                 } else {
-		                	 books = books + ", " + bookpos;
-		                 }
-		                 
-		             } 
-		             if(books.equals("")) { 
-	                 	u.setBooks(null);
-	                 	u.setDue(null);
-	                 	u.setCheckout(null);
-		             }else {
-		            	u.setBooks(books);
-		             }
-                 }  
-                 session.update(u);
-             }  
-             
-          } catch (HibernateException e) {
-             if (tx != null)
-                tx.rollback();
-             e.printStackTrace();
-          } finally {
-        	 tx.commit();
-             session.close();
-          }
-       }
+        try
+        {
+        	tx = session.beginTransaction();
+            List<?> usr = session.createQuery("FROM User").list();
+            
+            for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
+            {
+            	User u = (User) iterator.next();
+                
+            	if (u.getUserID().equals(UserID))
+                {
+            		System.out.println(u.getBooks());
+                	String bookin[] = u.getBooks().split(", ");
+                    String books = "";
+		            
+                    for (String bookpos : bookin)
+                    {
+                    	if (bookpos.equals(book))
+                    	{
+                    		bookpos = "";
+                    	} 
+		                if (bookpos.equals("") && books.equals(""))
+		                {
+		                	books = bookpos;
+		                }
+		                else
+		                {
+		                	books = books + ", " + bookpos;
+		                } 
+                    } 
+		            
+                    if(books.equals(""))
+                    { 
+                    	u.setBooks(null);
+                    	u.setDue(null);
+                    	u.setCheckout(null);
+                    }
+                    else
+                    {
+                    	u.setBooks(books);
+		            }
+                }
+            	
+                session.update(u);
+            }  
+        }
+        catch (HibernateException e)
+        {
+        	if (tx != null)
+        		tx.rollback();
+        	e.printStackTrace();
+        }
+        finally
+        {
+        	tx.commit();
+            session.close();
+        }
+	}
 	
 	public static List<User> overdue()
 	{
-		//java.time.LocalDate checkout = date.toLocalDate();
 		java.time.LocalDate today = LocalDate.now();
-		//System.out.println("=> " + today);
 		List<User> resultList = new ArrayList<User>();
 
 		Session session = getSessionFactory().openSession();
 		Transaction tx = null;
-		try {
-		   tx = session.beginTransaction();
-		   List<?> usr = session.createQuery("FROM User").list();
-		   java.time.LocalDate due;
-		   for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
-		   {
-		      User u = (User) iterator.next();
-		      //System.out.println("=> " + u.getDue());
+		try
+		{
+			tx = session.beginTransaction();
+			List<?> usr = session.createQuery("FROM User").list();
+			java.time.LocalDate due;
+			
+			for (Iterator<?> iterator = usr.iterator(); iterator.hasNext();) 
+			{
+				User u = (User) iterator.next();
 		      
-		      if (u.getDue() == null)
-		      {
-		    	  System.out.println("=> User " + u.getUserID() + "hasn't checked out any books!");
-		      }
-		      else
-		      {
-		    	  due = u.getDue().toLocalDate();
-			      if (due.compareTo(today) <= 0)
-			      {
-			    	  resultList.add(u);
-			      }
-		      }
-		   }
-		   tx.commit();
-		} catch (HibernateException e) {
-		   if (tx != null)
-		      tx.rollback();
-		   e.printStackTrace();
-		} finally {
-		   session.close();
+				if (u.getDue() == null)
+				{
+					System.out.println("=> User " + u.getUserID() + "hasn't checked out any books!");
+				}
+				else
+				{
+					due = u.getDue().toLocalDate();
+					
+					if (due.compareTo(today) <= 0)
+					{
+						resultList.add(u);
+					}
+				}
+			}
+			
+			tx.commit();
+		}
+		catch (HibernateException e)
+		{
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
 		}
 		
 		return resultList;
-
 	}
-	
-	
-	
-	
-	
 }
