ac1cc150e022580f50710398aa9cceeeba87c7e2
shellis0 <shellis0@e523dfcf-a971-48cd-b7b2-28d4b80018ea>
2022-04-27 01:19:24 +0000

    Cleaned up the organization of the data models and removed any unused import statements for the final project demo.

--- SemesterGroupProject/src/main/java/datamodel/Book.java
+++ SemesterGroupProject/src/main/java/datamodel/Book.java
@@ -16,7 +16,8 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "BookTable")
-public class Book {
+public class Book
+{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,87 +25,101 @@ public class Book {
 	private String bookid;
    
 	
-   @Column(name = "TITLE")
-   private String title;
+	@Column(name = "TITLE")
+	private String title;
 
-   @Column(name = "AUTHOR")
-   private String author;
+	@Column(name = "AUTHOR")
+	private String author;
    
-   @Column(name = "ISBN13")
-   private String isbn13;
+	@Column(name = "ISBN13")
+	private String isbn13;
    
-   @Column(name = "CATEGORY")
-   private String category;
+	@Column(name = "CATEGORY")
+	private String category;
    
-   @Column(name = "COPIES")
-   private Integer copies;
+	@Column(name = "COPIES")
+	private Integer copies;
    
-   @Column(name = "PublicationYear")
-   private Integer publicationyear;
+	@Column(name = "PublicationYear")
+	private Integer publicationyear;
    
-   public Book() {
-   }
+	public Book()
+	{}
 
-   public Book(String title, String author, String isbn13, String category, Integer copies, Integer publicationyear) {
-	   this.title = title;
-	   this.author = author;
-	   this.isbn13 = isbn13;
-	   this.category = category;
-	   this.copies = copies;
-	   this.publicationyear = publicationyear;
-   }
+	public Book(String title, String author, String isbn13, String category, Integer copies, Integer publicationyear)
+	{
+		this.title = title;
+		this.author = author;
+		this.isbn13 = isbn13;
+		this.category = category;
+		this.copies = copies;
+		this.publicationyear = publicationyear;
+	}
   
 
-   public String getTitle() {
-      return title;
-   }
+	public String getTitle()
+	{
+		return title;
+	}
 
-   public void setTitle(String title) {
-      this.title = title;
-   }
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
 
-   public String getAuthor() {
-      return author;
-   }
+	public String getAuthor()
+	{
+		return author;
+	}
 
-   public void setAuthor(String author) {
-      this.author = author;
-   }
+	public void setAuthor(String author)
+   	{
+		this.author = author;
+   	}
 
-   public String getISBN13() {
-      return isbn13;
-   }
+	public String getISBN13()
+	{
+		return isbn13;
+	}
 
-   public void setISBN13(String isbn13) {
-      this.isbn13 = isbn13;
-   }
+	public void setISBN13(String isbn13)
+	{
+		this.isbn13 = isbn13;
+	}
    
-   public String getCategory() {
-	   return category;
-   }
+	public String getCategory()
+	{
+		return category;
+	}
    
-   public void setCategory(String category) {
-	   this.category = category;
-   }
+	public void setCategory(String category)
+	{
+		this.category = category;
+	}
 
-   public Integer getCopies() {
-	   return copies;
-   }
+	public Integer getCopies()
+	{
+		return copies;
+	}
    
-   public void setCopies(Integer copies) {
-	   this.copies = copies;
-   }
+	public void setCopies(Integer copies)
+	{
+		this.copies = copies;
+	}
    
-   public Integer getPublicationYear() {
-	   return publicationyear;
-   }
+	public Integer getPublicationYear()
+	{
+		return publicationyear;
+	}
    
-   public void setPublicationYear(Integer publicationyear) {
-	   this.publicationyear = publicationyear;
-   }
+	public void setPublicationYear(Integer publicationyear)
+	{
+		this.publicationyear = publicationyear;
+	}
    
-   @Override
-   public String toString() {
-      return "Book: " + this.title + ", " + this.author + ", " + this.isbn13 + ", " + this.category + ", " + this.copies + ", " + this.publicationyear;
-   }
+	@Override
+	public String toString()
+	{
+		return "Book: " + this.title + ", " + this.author + ", " + this.isbn13 + ", " + this.category + ", " + this.copies + ", " + this.publicationyear;
+	}
 }
--- SemesterGroupProject/src/main/java/datamodel/LibrarianTable.java
+++ SemesterGroupProject/src/main/java/datamodel/LibrarianTable.java
@@ -16,57 +16,71 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "LibrarianTable")
-public class LibrarianTable {
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class LibrarianTable
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
    
-   @Column(name = "id")
-   private Integer id;
+	@Column(name = "id")
+	private Integer id;
   
-   @Column(name = "BRANCH")
-   private String branch;
+	@Column(name = "BRANCH")
+	private String branch;
  
-   @Column(name = "PIN")
-   private String pin;
-   public LibrarianTable() {
-	   super();
-   }
-   public LibrarianTable(Integer id, String branch, String pin) {
-      this.id = id;
-      this.branch = branch;
-      this.pin = pin;
-   }
-   public LibrarianTable(String branch, String pin) {
-	  this.branch = branch; 
-      this.pin = pin;
-   }
+	@Column(name = "PIN")
+	private String pin;
+   
+	public LibrarianTable()
+	{
+		super();
+	}
+   
+	public LibrarianTable(Integer id, String branch, String pin)
+	{
+		this.id = id;
+		this.branch = branch;
+		this.pin = pin;
+	}
+	
+	public LibrarianTable(String branch, String pin)
+	{
+		this.branch = branch; 
+		this.pin = pin;
+	}
 
-   public Integer getId() {
-      return id;
-   }
+	public Integer getId()
+	{
+		return id;
+	}
 
-   public void setId(Integer id) {
-      this.id = id;
-   }
+	public void setId(Integer id)
+	{
+		this.id = id;
+	}
   
-   public String getbranch() {
-	      return this.branch;
-	   }
+	public String getbranch()
+	{
+		return this.branch;
+	}
 
-   public void setBranchName(String branch) {
-	      this.branch = branch;
-	   }
-   public String getPin() {
-      return pin;
-   }
+	public void setBranchName(String branch)
+	{
+		this.branch = branch;
+	}
+   
+	public String getPin()
+	{
+		return pin;
+	}
 
-   public void setPin(String pin) {
-      this.pin = pin;
-   }
+	public void setPin(String pin)
+	{
+		this.pin = pin;
+	}
      
-   @Override
-   public String toString() {
-      return "login info: " + this.branch + ", " + this.pin;
-   }
+	@Override
+	public String toString()
+	{
+		return "login info: " + this.branch + ", " + this.pin;
+	}
 }
-
